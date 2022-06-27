862ee5b604454bf0568e7c7ec966514280bfd566
Zahari Petkov <zahari@balena.io>
2022-06-27 16:19:14 +0300

    Extract CLI and ENV parsing in separate struct
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,15 @@ use nix::unistd::{getpid, Pid};
 use std::{env, process, thread, time};
 use systemd::daemon;
 
-fn print_usage(opts: &Options) {
-    println!("{}", opts.usage(&opts.short_usage("healthdog")));
+struct Args {
+    pid: Option<pid_t>,
+    healthcheck: String,
+    help: bool,
+    free: Vec<String>,
+    interval: Option<time::Duration>,
 }
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-
     let mut opts = Options::new();
 
     opts.parsing_style(ParsingStyle::StopAtFirstFree)
@@ -27,82 +29,98 @@ fn main() -> Result<()> {
         .reqopt("c", "healthcheck", "Set healthcheck command", "COMMAND")
         .optflag("h", "help", "Print this help menu");
 
-    let matches = match opts.parse(&args) {
-        Ok(m) => m,
+    let args = match populate_args(&opts) {
+        Ok(args) => args,
         Err(err) => {
             print_usage(&opts);
-            bail!(err)
+            bail!(err);
         }
     };
 
-    if matches.opt_present("h") {
+    if args.help {
         print_usage(&opts);
         return Ok(());
     }
 
-    let health_cmd = matches.opt_str("healthcheck").unwrap();
+    if let Some(pid) = args.pid {
+        let interval = if let Some(interval) = args.interval {
+            interval
+        } else {
+            println!("WATCHDOG_USEC not set");
+            println!("Exiting, will not pet the watchdog");
+            return Ok(());
+        };
 
-    let interval = watchdog_interval()?;
-
-    match matches.opt_str("pid") {
-        Some(pid) => {
-            let interval = match interval {
-                None => {
-                    println!("Exiting, will not pet the watchdog");
-                    return Ok(());
-                }
-                Some(value) => value,
-            };
-
-            let pid = match pid.parse::<pid_t>() {
-                Ok(pid) => pid,
-                Err(err) => {
-                    print_usage(&opts);
-                    bail!(err)
-                }
-            };
-
-            loop {
-                if kill(Pid::from_raw(pid), None).is_err() {
-                    println!("Parent process exited");
-                    return Ok(());
-                }
-
-                let status = process::Command::new(&health_cmd).status()?;
-
-                if status.success() {
-                    let message = [("WATCHDOG", "1")];
-                    daemon::pid_notify(pid, false, message.iter())?;
-                }
-
-                thread::sleep(interval);
+        loop {
+            if kill(Pid::from_raw(pid), None).is_err() {
+                println!("Parent process exited");
+                return Ok(());
             }
+
+            let status = process::Command::new(&args.healthcheck).status()?;
+
+            if status.success() {
+                let message = [("WATCHDOG", "1")];
+                daemon::pid_notify(pid, false, message.iter())?;
+            }
+
+            thread::sleep(interval);
         }
-        None => {
-            let pid = getpid();
+    } else {
+        let pid = getpid();
 
-            // first we start the helper child process that will run the
-            // healthcheck
-            let mut helper = process::Command::new("/proc/self/exe")
-                .args(&["--healthcheck", &health_cmd])
-                .args(&["--pid", pid.to_string().as_str()])
-                .spawn()?;
+        // First we start the helper child process that will run the
+        // healthcheck
+        let mut helper = process::Command::new("/proc/self/exe")
+            .args(&["--healthcheck", &args.healthcheck])
+            .args(&["--pid", pid.to_string().as_str()])
+            .spawn()?;
 
-            // Then we execve to the requested program
-            let err = exec::Command::new(&matches.free[0])
-                .args(&matches.free[1..])
-                .exec();
+        // Then we execve to the requested program
+        let err = exec::Command::new(&args.free[0])
+            .args(&args.free[1..])
+            .exec();
 
-            helper.kill().unwrap_or(());
+        helper.kill().unwrap_or(());
 
-            bail!(err)
-        }
+        bail!(err)
     }
+}
+
+fn populate_args(opts: &Options) -> Result<Args> {
+    let cli_args: Vec<String> = env::args().skip(1).collect();
+    let matches = opts.parse(&cli_args)?;
+
+    let pid = matches
+        .opt_str("pid")
+        .map(|pid| pid.parse::<pid_t>())
+        .transpose()?;
+
+    // Healthcheck is a required option, so it is safe to unwrap.
+    let healthcheck = matches.opt_str("healthcheck").unwrap();
+
+    let help = matches.opt_present("h");
+
+    let free = matches.free;
+
+    let interval = get_watchdog_interval()?;
+
+    Ok(Args {
+        pid,
+        healthcheck,
+        help,
+        free,
+        interval,
+    })
+}
+
+fn print_usage(opts: &Options) {
+    println!("{}", opts.usage(&opts.short_usage("healthdog")));
 }
 
 /// Returns the watchdog interval duration. Returns `Option::None` when we are
 /// not supposed to pet the watchdog.
-fn watchdog_interval() -> Result<Option<time::Duration>> {
+fn get_watchdog_interval() -> Result<Option<time::Duration>> {
     match env::var("WATCHDOG_USEC") {
         Ok(val) => match val.parse::<u64>().ok() {
             Some(usec) => Ok(Some(time::Duration::from_micros(usec / 2))),
@@ -110,7 +128,6 @@ fn watchdog_interval() -> Result<Option<time::Duration>> {
         },
         Err(err) => {
             if err == env::VarError::NotPresent {
-                println!("WATCHDOG_USEC not set");
                 Ok(Option::None)
             } else {
                 bail!("Error reading WATCHDOG_USEC: {}", err);
